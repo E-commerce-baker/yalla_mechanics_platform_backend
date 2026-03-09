0a458fe211358212dae9da556bb45f23eff8f462
baker-7-10 <bakerdes654@gmail.com>
2026-03-10 00:47:45 +0300

    fix

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
